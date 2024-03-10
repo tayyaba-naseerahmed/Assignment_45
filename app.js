7027f8934b71a65e03fdd53330fffe3ec9f7391b
Tayyaba Naseer Ahmed <tayyabanaseerahmed6@gmail.com>
2024-03-11 00:59:11 +0500

    38.Cities

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 "use strict";
+function describe_city(city, country = 'Turkey') {
+    console.log(`${city}  is in ${country}`);
+}
+describe_city('Istanbul');
+describe_city(' Gaza', 'Palestine');
+describe_city('Karachi', 'Pakistan');
---
+++ app.ts
@@ -0,0 +1,7 @@
+function describe_city(city: string, country: string = 'Turkey'){
+        console.log(`${city}  is in ${country}`)
+}
+describe_city('Istanbul');
+describe_city(' Gaza' , 'Palestine');
+describe_city('Karachi' , 'Pakistan');
+   
