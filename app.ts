7476c94b10b21ef8d8b0ff3b18120b22f0b93340
Tayyaba Naseer Ahmed <tayyabanaseerahmed6@gmail.com>
2024-03-11 00:51:57 +0500

    32.Checking_Username

--- app.ts
+++ app.ts
@@ -1,17 +1,27 @@
+//Do the following to create a program that simulates
+// how websites ensure that everyone has a unique username.
 
-// Add an if test to Exercise 28 to make sure the list of users is not empty.
+//• Make a list of five or more usernames called current_users.
 
-//• If the list is empty, print the message We need to find some users!
+//• Make another list of five usernames called new_users.
+// Make sure one or two of the new usernames are also in the current_users list.
 
-//• Remove all of the usernames from your array, and make sure the correct message is printed.
+//• Loop through the new_users list to see if each new username has already been used. 
+//If it has, print a message that the person will need to enter a
+//new username. If a username has not been used, print a message saying that the username is available.
+ let new_users = ["faris","hashim","saadi","mahir","salar"]
 
+ for(let i=0; i < new_users.length; i++){
 
-let users=[];
-
-if(users.length >0){
-    for(var i = 0 ; i <users.length ; i++){
-        console.log("Hello," + users [i])
-    }
-}else{ 
-    console.log("We need to find some users! ");
+    if (new_users[i] == "jehan sikandar")
+    {console.log(`This username ${new_users[i].toLowerCase().toUpperCase()} is not available`)}
+    
+    else if(new_users[i] == "birbal")
+    {console.log(`This username ${new_users[i].toLowerCase().toUpperCase()} is not available`)}
+    
+    else
+    
+    (console.log(`This username ${new_users[i].toLowerCase().toUpperCase()} is avaliable`))
 }
+
+  
