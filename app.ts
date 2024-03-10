7bef79e37b1cc013938430b0c0db4c69e84cb8df
Tayyaba Naseer Ahmed <tayyabanaseerahmed6@gmail.com>
2024-03-11 00:55:32 +0500

    34.Pizzas

--- app.ts
+++ app.ts
@@ -1,31 +1,13 @@
-//Ordinal numbers indicate their position in a array, such as 1st or 2nd.
-// Most ordinal numbers end in th, except 1, 2, and 3.
+let favPizzas = ["margherita","fajita","texas"];
 
-//• Store the numbers 1 through 9 in a array.
-
-//• Loop through the array.
-
-//• Use an if-else chain inside the loop to print the proper ordinal ending for each number.
-//Your output should read "1st 2nd 3rd 4th 5th 6th
-//7th 8th 9th", and each result should be on a separate line.
-
-
-
-
-
-let numbers =[1,2,3,4,5,6,7,8,9];
-
-for (var i= 0; i < numbers.length; i ++){
-    let number = numbers[i]
-var a;
-    if (number === 1){
-        a ="st"
-    }else if (number === 2){
-        a = "nd"
-    }else if (number === 3){
-        a="rd"
-    }else {
-        a= "th"
-    }
-    console.log(number + a);
+console.log("Pizza Names:");
+for (var i = 0 ; i < favPizzas.length ; i++){
+    console.log(favPizzas[i]);
 }
+console.log("\nStatements about my favorite pizzas:");
+
+for (var i = 0 ; i < favPizzas.length ; i++){
+    console.log("i like " + favPizzas[i] + "pizzas:")
+}
+
+console.log("\nI really love pizza!");
