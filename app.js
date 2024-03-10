1f6fa0e977f29918c95b427535cf45f5b12dcfe9
Tayyaba Naseer Ahmed <tayyabanaseerahmed6@gmail.com>
2024-03-11 00:16:34 +0500

    15.Changing_Guest_List

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 "use strict";
 let guest_list = ['mehmet', 'umar', 'mosa', 'tasmia', 'uzma'];
 for (let i = 0; i < guest_list.length; i++) {
-    console.log('Respected Sir/Madam' + guest_list[i] + ',\nWe invite you on dinner tomorrow.\n\nThankyou');
+    console.log('Respected Sir/Madam ' + guest_list[i] + ',\nWe invite you on dinner tomorrow.\nThankyou\n');
+}
+let not_present = 'Tasmia';
+let new_guest = 'Motaz Azaiza';
+guest_list[3] = new_guest;
+for (let i = 0; i < guest_list.length; i++) {
+    console.log('Respected Sir/Madam' + guest_list[i] + '\nThank You \n');
+}
+{
+    console.log(`Miss.${not_present} 'will not coming for tomorrow dinner.`);
 }
--- app.ts
+++ app.ts
@@ -1,4 +1,12 @@
-let guest_list: string[]= ['mehmet','umar','mosa','tasmia','uzma'];
-for(let i=0; i<guest_list.length;i++){
-    console.log('Respected Sir/Madam' + guest_list [i] + ',\nWe invite you on dinner tomorrow.\n\nThankyou')
-}
+let guest_list : string[]= ['mehmet','umar','mosa','tasmia','uzma'];
+ for(let i=0; i<guest_list.length; i++){
+    console.log('Respected Sir/Madam ' + guest_list [i] + ',\nWe invite you on dinner tomorrow.\nThankyou\n')
+   }
+let not_present : string ='Tasmia';
+let new_guest : string ='Motaz Azaiza';
+guest_list[3]= new_guest
+for(let i=0; i<guest_list.length; i++){
+console.log('Respected Sir/Madam' + guest_list [i] + '\nThank You \n')
+ }{
+    console.log(`Miss.${not_present} 'will not coming for tomorrow dinner.`)
+ }
