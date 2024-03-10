8e453a099a1e9f983e628205438d1b8904752ca3
Tayyaba Naseer Ahmed <tayyabanaseerahmed6@gmail.com>
2024-03-11 01:09:31 +0500

    43.Unchanged Magicians

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 "use strict";
-//Great Magicians: Start with a copy of your program from Exercise 39.
-// Write a function called make_great() that modifies the array of magicians by adding
-// the phrase the Great to each magician’s name. Call show_magicians() to
-// see that the list has actually been modified.
-function show_magician(Magician) {
-    for (var i = 0; i < magicianNames.length; i++) {
-        console.log("Magician Name : " + magicianNames[i]);
+//Unchanged Magicians: Start with your work from Exercise 40. Call the
+//function make_great() with a copy of the array of magicians’ names. Because the
+//original array will be unchanged, return the new array and store it in a separate array.
+//Call show_magicians() with each array to show that you have one array of the original
+//names and one array with the Great added to each magician’s name.
+function show_magicians(magicians) {
+    for (let magician of magicians) {
+        console.log(magician.charAt(0).toUpperCase() + magician.slice(1));
     }
 }
-let magicianNames = ["Professor Snape", "Albus DumbleDoor", "Tom Riddle"];
-show_magician(magicianNames);
+function make_great(magician) {
+    for (let i = 0; i < show_magicians.length; i++) {
+        magician[i] = magician[i] + 'The Great';
+    }
+}
+function make_great2(magicians) {
+    let Greatmagicians = [];
+    for (let i = 0; i < magicians.length; i++) {
+        Greatmagicians.push(magicians[i] + 'The Great');
+    }
+    return Greatmagicians;
+}
+let magicians2 = ['Professor Snape ', 'Tom Riddle', 'Albus DumbalDoor'];
+let magicians3 = make_great2(magicians2);
+show_magicians(magicians2);
+show_magicians(magicians2);
--- app.ts
+++ app.ts
@@ -1,13 +1,29 @@
-//Great Magicians: Start with a copy of your program from Exercise 39.
-// Write a function called make_great() that modifies the array of magicians by adding
-// the phrase the Great to each magician’s name. Call show_magicians() to
-// see that the list has actually been modified.
+//Unchanged Magicians: Start with your work from Exercise 40. Call the
+//function make_great() with a copy of the array of magicians’ names. Because the
+//original array will be unchanged, return the new array and store it in a separate array.
+//Call show_magicians() with each array to show that you have one array of the original
+//names and one array with the Great added to each magician’s name.
 
-function show_magician(Magician) {
-    for(var i = 0 ; i < magicianNames.length ; i++){
-        console.log("Magician Name : " + magicianNames[i])
+function show_magicians( magicians: string[]) {
+    for ( let magician of magicians) {
+        console.log( magician.charAt(0). toUpperCase() + magician.slice(1))
     }
     }
-    let  magicianNames = ["Professor Snape", "Albus DumbleDoor" , "Tom Riddle"];
+function make_great( magician: string [] ) {
+    for (let i = 0; i < show_magicians.length; i++){
+        magician[i] = magician[i] +  'The Great'
+    }
+}
+function make_great2( magicians:string[]): string [] {
+    let Greatmagicians: string[] = [] ;
+    for ( let i = 0 ; i < magicians.length ; i ++){
+        Greatmagicians.push(magicians [i] + 'The Great' )
+    }
+    return Greatmagicians ;
+}
 
-    show_magician(magicianNames);
+let magicians2 : string [] = ['Professor Snape ' , 'Tom Riddle' , 'Albus DumbalDoor']
+  let magicians3 : string [] =  make_great2(magicians2)
+  show_magicians(magicians2);
+  show_magicians(magicians2);
+  
