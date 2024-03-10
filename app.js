92372e480f3906ac623baadf967f0659c4ae1597
Tayyaba Naseer Ahmed <tayyabanaseerahmed6@gmail.com>
2024-03-11 01:10:17 +0500

    42.Sandwich

--- app.js
+++ app.js
@@ -1,27 +1,13 @@
 "use strict";
-//Unchanged Magicians: Start with your work from Exercise 40. Call the
-//function make_great() with a copy of the array of magicians’ names. Because the
-//original array will be unchanged, return the new array and store it in a separate array.
-//Call show_magicians() with each array to show that you have one array of the original
-//names and one array with the Great added to each magician’s name.
-function show_magicians(magicians) {
-    for (let magician of magicians) {
-        console.log(magician.charAt(0).toUpperCase() + magician.slice(1));
+function sandwich(flavour) {
+    console.log('sandwich Order:');
+    for (let i = 0; i < flavour.length; i++) {
+        console.log(`${flavour[i]}`);
     }
 }
-function make_great(magician) {
-    for (let i = 0; i < show_magicians.length; i++) {
-        magician[i] = magician[i] + 'The Great';
-    }
-}
-function make_great2(magicians) {
-    let Greatmagicians = [];
-    for (let i = 0; i < magicians.length; i++) {
-        Greatmagicians.push(magicians[i] + 'The Great');
-    }
-    return Greatmagicians;
-}
-let magicians2 = ['Professor Snape ', 'Tom Riddle', 'Albus DumbalDoor'];
-let magicians3 = make_great2(magicians2);
-show_magicians(magicians2);
-show_magicians(magicians2);
+console.log('This order is for First Customer');
+sandwich([' Grilled chicken', 'olive & cheese']);
+console.log('This order is for Second Customer');
+sandwich(['Ice cream', 'Roasted Beef']);
+console.log('This order is for Third Customer');
+sandwich(['club', 'Panini']);
--- app.ts
+++ app.ts
@@ -1,29 +1,15 @@
-//Unchanged Magicians: Start with your work from Exercise 40. Call the
-//function make_great() with a copy of the array of magicians’ names. Because the
-//original array will be unchanged, return the new array and store it in a separate array.
-//Call show_magicians() with each array to show that you have one array of the original
-//names and one array with the Great added to each magician’s name.
+function sandwich (flavour : string [] ) {
+    console.log('sandwich Order:')
 
-function show_magicians( magicians: string[]) {
-    for ( let magician of magicians) {
-        console.log( magician.charAt(0). toUpperCase() + magician.slice(1))
-    }
-    }
-function make_great( magician: string [] ) {
-    for (let i = 0; i < show_magicians.length; i++){
-        magician[i] = magician[i] +  'The Great'
-    }
+for(let i = 0 ; i < flavour.length ; i++ ) {
+    console.log(`${ flavour [i] }` )
 }
-function make_great2( magicians:string[]): string [] {
-    let Greatmagicians: string[] = [] ;
-    for ( let i = 0 ; i < magicians.length ; i ++){
-        Greatmagicians.push(magicians [i] + 'The Great' )
-    }
-    return Greatmagicians ;
 }
+console.log('This order is for First Customer')
+sandwich([' Grilled chicken', 'olive & cheese']);
 
-let magicians2 : string [] = ['Professor Snape ' , 'Tom Riddle' , 'Albus DumbalDoor']
-  let magicians3 : string [] =  make_great2(magicians2)
-  show_magicians(magicians2);
-  show_magicians(magicians2);
-  
+console.log('This order is for Second Customer')
+sandwich(['Ice cream','Roasted Beef']);
+
+console.log('This order is for Third Customer')
+sandwich(['club','Panini']);
