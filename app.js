17fe2e45d3d86e361b4d0acadb916d4f38ea4856
Tayyaba Naseer Ahmed <tayyabanaseerahmed6@gmail.com>
2024-03-11 00:48:08 +0500

    29.Favorite Fruit

--- app.js
+++ app.js
@@ -1,117 +1,22 @@
 "use strict";
-//Write an if-else chain that determines a person’s stage of life. 
-//Set a value for the variable age, and then:
-//• If the person is less than 2 years old, print a message that the person is a baby.
-let age = 1;
-if (age < 2) {
-    console.log("person is a baby.");
+//Make a array of your favorite fruits, and then write a series of independent 
+//if statements that check for certain fruits in your array.
+//• Make a array of your three favorite fruits and call it favorite_fruits.
+//• Write five if statements. Each should check whether a certain kind of fruit is in your array. 
+//If the fruit is in your array, the if block should print a statement,such as You really like bananas!
+let favorite_fruits = ['banana', 'guava', 'mango'];
+if (favorite_fruits.includes('banana')) {
+    console.log('i really like banana.');
 }
-else if (age < 4) {
-    console.log("person is a toddler.");
+if (favorite_fruits.includes('guava')) {
+    console.log('i really like guava.');
 }
-else if (age < 13) {
-    console.log("person is a kid.");
+if (favorite_fruits.includes('mango')) {
+    console.log('i really like mango.');
 }
-else if (age < 20) {
-    console.log("person is a teenager.");
+if (favorite_fruits.includes('strawberry')) {
+    console.log('i really like strawberry.');
 }
-else if (age < 65) {
-    console.log("person is an adult.");
+if (favorite_fruits.includes('orange')) {
+    console.log('i really like orange.');
 }
-else
-    console.log("person is an elder.");
-//• If the person is at least 2 years old but less than 4, print a message that the person is a toddler.let age: number = 1;
-age = 3;
-if (age < 2) {
-    console.log("person is a baby.");
-}
-else if (age < 4) {
-    console.log("person is a toddler.");
-}
-else if (age < 13) {
-    console.log("person is a kid.");
-}
-else if (age < 20) {
-    console.log("person is a teenager.");
-}
-else if (age < 65) {
-    console.log("person is an adult.");
-}
-else
-    console.log("person is an elder.");
-//• If the person is at least 4 years old but less than 13, print a message that the person is a kid.let age: number = 1;
-age = 7;
-if (age < 2) {
-    console.log("person is a baby.");
-}
-else if (age < 4) {
-    console.log("person is a toddler.");
-}
-else if (age < 13) {
-    console.log("person is a kid.");
-}
-else if (age < 20) {
-    console.log("person is a teenager.");
-}
-else if (age < 65) {
-    console.log("person is an adult.");
-}
-else
-    console.log("person is an elder.");
-//• If the person is at least 13 years old but less than 20, print a message that the person is a teenagerlet age: number = 1;
-age = 15;
-if (age < 2) {
-    console.log("person is a baby.");
-}
-else if (age < 4) {
-    console.log("person is a toddler.");
-}
-else if (age < 13) {
-    console.log("person is a kid.");
-}
-else if (age < 20) {
-    console.log("person is a teenager.");
-}
-else if (age < 65) {
-    console.log("person is an adult.");
-}
-else
-    console.log("person is an elder.");
-//• If the person is at least 20 years old but less than 65, print a message that the person is an adult.
-age = 30;
-if (age < 2) {
-    console.log("person is a baby.");
-}
-else if (age < 4) {
-    console.log("person is a toddler.");
-}
-else if (age < 13) {
-    console.log("person is a kid.");
-}
-else if (age < 20) {
-    console.log("person is a teenager.");
-}
-else if (age < 65) {
-    console.log("person is an adult.");
-}
-else
-    console.log("person is an elder.");
-//• If the person is age 65 or older, print a message that the person is an elder.
-age = 70;
-if (age < 2) {
-    console.log("person is a baby.");
-}
-else if (age < 4) {
-    console.log("person is a toddler.");
-}
-else if (age < 13) {
-    console.log("person is a kid.");
-}
-else if (age < 20) {
-    console.log("person is a teenager.");
-}
-else if (age < 65) {
-    console.log("person is an adult.");
-}
-else
-    console.log("person is an elder.");
