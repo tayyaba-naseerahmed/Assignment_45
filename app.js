19b059f1c838363e69ba8e073340172f9518e112
Tayyaba Naseer Ahmed <tayyabanaseerahmed6@gmail.com>
2024-03-11 01:02:44 +0500

    41.Magicians

--- app.js
+++ app.js
@@ -1,23 +1,8 @@
 "use strict";
-//Album: Write a function called make_album() that builds a Object
-//describing a music album. The function should take in an artist name and an
-//album title, and it should return a Object containing these two pieces of
-//information. Use the function to make three dictionaries representing different
-//albums. Print each return value to show that Objects are storing the
-//album information correctly.
-//Add an optional parameter to make_album() that allows you to store the
-//number of tracks on an album. If the calling line includes a value for the number
-//of tracks, add that value to the album's object. make at least one new function call 
-// that includes the number of tracks on an album.
-let make_album = (artist, Album, tracks, , { let, albumReturn: Albums = {}, artist: artist, charAt }) => ;
-(0).toUpperCase() + artist.slice(1),
-    album_title;
-album_title.charAt(0).toUpperCase() + album_title.slice(1);
-return Object;
-let album = (make_album("Muhammad Al Muqit", "Inspiring Nasheeds"));
-console.log(make_album);
-album = (make_album("Maher Zain", "Thank You Allah"));
-if (tracks !== undefined) {
-    albumReturn.track = tracks;
+function show_magicians(magicians) {
+    for (let magician of magicians) {
+        console.log(magician.charAt(0).toUpperCase() + magician.slice(1));
+    }
 }
-return album;
+let magicians_names = ["Professor Snape", "Albus DumbalDoor", "Tom Riddle"];
+show_magicians(magicians_names);
--- app.ts
+++ app.ts
@@ -1,34 +1,10 @@
-//Album: Write a function called make_album() that builds a Object
-//describing a music album. The function should take in an artist name and an
-//album title, and it should return a Object containing these two pieces of
-//information. Use the function to make three dictionaries representing different
-//albums. Print each return value to show that Objects are storing the
-//album information correctly.
-//Add an optional parameter to make_album() that allows you to store the
-//number of tracks on an album. If the calling line includes a value for the number
-//of tracks, add that value to the album's object. make at least one new function call 
-// that includes the number of tracks on an album.
-let make_album=(artist: string , Album: string, tracks?: number') Album =>
-{let albumReturn: Albums =
-    {
-    }
+function show_magicians(magicians : string []){
+   for( let magician of magicians) {
 
-        artist: artist.charAt(0).toUpperCase() + artist.slice(1),
-            album_title: album_title.charAt(0).toUpperCase() + album_title.slice(1)
-    }
-    return Object;
+    console.log(magician.charAt(0).toUpperCase() + magician.slice(1))
+   }
 }
-    let album =(make_album("Muhammad Al Muqit","Inspiring Nasheeds"));
+  
+let magicians_names = ["Professor Snape" , "Albus DumbalDoor" , "Tom Riddle"]
 
-    console.log(make_album);
-
-    album =(make_album("Maher Zain","Thank You Allah"));
-
-    type Albums = {
-        artist : string ;
-        album  : string ;
-        track? : number 
-    }
-
-    if ( tracks !== undefined) {albumReturn.track = tracks}
-    return album}
+show_magicians(magicians_names);
