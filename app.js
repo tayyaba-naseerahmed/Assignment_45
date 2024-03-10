d54c8804e6e7ea0f9114473b9a8b4b037725ba11
Tayyaba Naseer Ahmed <tayyabanaseerahmed6@gmail.com>
2024-03-11 01:01:42 +0500

    40.Albums

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 "use strict";
-function city_country(city, country) {
-    return (`"${city}, ${country}"`);
+//Album: Write a function called make_album() that builds a Object
+//describing a music album. The function should take in an artist name and an
+//album title, and it should return a Object containing these two pieces of
+//information. Use the function to make three dictionaries representing different
+//albums. Print each return value to show that Objects are storing the
+//album information correctly.
+//Add an optional parameter to make_album() that allows you to store the
+//number of tracks on an album. If the calling line includes a value for the number
+//of tracks, add that value to the album's object. make at least one new function call 
+// that includes the number of tracks on an album.
+let make_album = (artist, Album, tracks, , { let, albumReturn: Albums = {}, artist: artist, charAt }) => ;
+(0).toUpperCase() + artist.slice(1),
+    album_title;
+album_title.charAt(0).toUpperCase() + album_title.slice(1);
+return Object;
+let album = (make_album("Muhammad Al Muqit", "Inspiring Nasheeds"));
+console.log(make_album);
+album = (make_album("Maher Zain", "Thank You Allah"));
+if (tracks !== undefined) {
+    albumReturn.track = tracks;
 }
-let F1 = city_country('Istanbul', 'Turkey');
-let F2 = city_country('Gaza', 'Palestine');
-let F3 = city_country('Karachi', 'Pakistan');
-console.log(F1);
-console.log(F2);
-console.log(F3);
+return album;
--- app.ts
+++ app.ts
@@ -1,10 +1,34 @@
-function city_country(city: string,country: string) {
-    return (`"${city}, ${country}"`)
+//Album: Write a function called make_album() that builds a Object
+//describing a music album. The function should take in an artist name and an
+//album title, and it should return a Object containing these two pieces of
+//information. Use the function to make three dictionaries representing different
+//albums. Print each return value to show that Objects are storing the
+//album information correctly.
+//Add an optional parameter to make_album() that allows you to store the
+//number of tracks on an album. If the calling line includes a value for the number
+//of tracks, add that value to the album's object. make at least one new function call 
+// that includes the number of tracks on an album.
+let make_album=(artist: string , Album: string, tracks?: number') Album =>
+{let albumReturn: Albums =
+    {
     }
-    let  F1 = city_country('Istanbul' , 'Turkey');
-    let  F2 = city_country('Gaza' , 'Palestine');
-    let  F3 = city_country('Karachi' , 'Pakistan');
 
-    console.log(F1);
-    console.log(F2);
-    console.log(F3);
+        artist: artist.charAt(0).toUpperCase() + artist.slice(1),
+            album_title: album_title.charAt(0).toUpperCase() + album_title.slice(1)
+    }
+    return Object;
+}
+    let album =(make_album("Muhammad Al Muqit","Inspiring Nasheeds"));
+
+    console.log(make_album);
+
+    album =(make_album("Maher Zain","Thank You Allah"));
+
+    type Albums = {
+        artist : string ;
+        album  : string ;
+        track? : number 
+    }
+
+    if ( tracks !== undefined) {albumReturn.track = tracks}
+    return album}
