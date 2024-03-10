ffa75af209be37d850c4a99d73a17fa1a94916b3
Tayyaba Naseer Ahmed <tayyabanaseerahmed6@gmail.com>
2024-03-11 00:03:10 +0500

    9. Favorite Number.

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
 "use strict";
-// Write addition, subtraction, multiplication, and division operations that each result 
-//in the number 8. Be sure to enclose your operations in print statements to see the results. 
-//8. You should create four lines that look like this:
-console.log(5 + 3);
-console.log(11 - 3);
-console.log(4 * 2);
-console.log(16 / 2);
+let favoriteNumber = 313;
+console.log(`my favorite number is ${favoriteNumber}`);
