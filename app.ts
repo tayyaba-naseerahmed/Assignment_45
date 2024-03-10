640229307a66b99491eecf98b22a31902fded5da
Tayyaba Naseer Ahmed <tayyabanaseerahmed6@gmail.com>
2024-03-11 00:41:59 +0500

    25. Alien Color #1

--- app.ts
+++ app.ts
@@ -1,78 +1,16 @@
-let car: string = 'subaru';
-let age: number = 25;
-let numbers: number[] = [1, 2, 3, 4,];
+// Crete a variable called alien_color
+let alien_color: string = "green";
 
-// Test 1: Equality (True)
-console.log("Is car == 'subaru'? I predict True.");
-console.log(car == 'subaru'); // True (case-insentive)
+// Write an if statement to test whether the alien's color is green. if it is,
+//  print a message that the player just earned 5 points.
+if(alien_color == "green"){
+    console.log("the player just earned 5 points.");
+}
 
-// Test 2: Strict equality (False)
-console.log("Is car === 'subaru'? I predict False.");
-console.log(car === 'subaru'); // False (case-insentive)
+//• Write one version of this program that passes the if test and another that fails.
+// (The version that fails will have no output.)
 
-//Test 3: Inequality (True)
-console.log("Is car != 'Toyota'? I predict True.");
-console.log(car != 'Toyoto') //True
-
-//Test 4: Inequality (False)
-console.log("Is car !== 'subaru'); I predict False.");
-console.log(car !== 'subaru'); // False (case-insentive)
-
-
-//**Lowercase Function Tests **
-
-// Test 5: Lowercase conversion (True)
-console.log("Is car.toLowerCase() =='subaru'? I predict True.");
-console.log(car.toLowerCase() == 'subaru'); // True (converted to lowercase)
-
-// Test 6: Lowercase conversion (False) 
-console.log("Is car === car.toLowerCase()? I predict False.");
-console.log(car === car.toLowerCase()); // True (converted to lowercase)
-
-//   **Numerial Tests**
-
-// Test 7: Equality (True)
-console.log("Is age == 25? I predict True.");
-console.log(age == 25); // True 
-
-// Test 8: Inequality (False)
-console.log("Is age != 30? I predict True.");
-console.log(age != 30); // True 
-
-// Test 9: Greater than (False)
-console.log("Is age > 30? I predict False.");
-console.log(age > 30); // False
-
-// Test 10: Less than or equal (True)
-console.log("Is age <= 25? I predict True.");
-console.log(age <= 25); // True 
-
-// **Logical Operators**
-
-// Test 11: AND (True)
-console.log("Is age > 20 && age < 30? I predict True.");
-console.log(age > 20 && age < 30); // True  ( both conditions met)
-
-// Test 12: OR (False)
-console.log("Is age > 30 || age < 18? I predict False.");
-console.log(age > 30 || age < 18); // False ( neither conditions met)
-
-// **Array Tests**
-// Test 13: In array (True)
-console.log("Is 3 in numbers? I predict True.");
-console.log(3  in numbers); // True (checks for index existence)
-
-// Test 14: Not in array (False)
-console.log(5  in numbers); // True (negation of "in" operator)
-
-
-
-
-
-
-
-
-
-
-
-
+alien_color = "red";
+if(alien_color == "green"){
+    console.log("the player just earned 5 points.");
+}
