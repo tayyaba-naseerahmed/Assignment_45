504b91ebb904369c1b928eb562f4de82c4387678
Tayyaba Naseer Ahmed <tayyabanaseerahmed6@gmail.com>
2024-03-11 00:50:17 +0500

    31.No_Users

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 "use strict";
-// Make a array of five or more usernames, including the name 'admin'. 
-//Imagine you are writing code that will print a greeting to each user after they log in to a website.
-// Loop through the array, and print a greeting to each user:
-//• If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
-//• Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.
-let username = ["admin", "aqib", "jehan", "salar", "faris", "hashim"];
-for (var i = 0; i < username.length; i++) {
-    if (username[i] === "Admin") {
-        console.log("hello admin, would you like to see a status report?");
+// Add an if test to Exercise 28 to make sure the list of users is not empty.
+//• If the list is empty, print the message We need to find some users!
+//• Remove all of the usernames from your array, and make sure the correct message is printed.
+let users = [];
+if (users.length > 0) {
+    for (var i = 0; i < users.length; i++) {
+        console.log("Hello," + users[i]);
     }
-    else {
-        console.log("hello" + username[i] + ". thank you for logging in again.");
-    }
+}
+else {
+    console.log("We need to find some users! ");
 }
--- app.ts
+++ app.ts
@@ -1,17 +1,17 @@
-// Make a array of five or more usernames, including the name 'admin'. 
-//Imagine you are writing code that will print a greeting to each user after they log in to a website.
-// Loop through the array, and print a greeting to each user:
 
-//• If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
+// Add an if test to Exercise 28 to make sure the list of users is not empty.
 
-//• Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.
+//• If the list is empty, print the message We need to find some users!
 
-let username=["admin","aqib","jehan","salar","faris","hashim"];
-for (var i=0; i<username.length ;  i++){
-    if(username[i] === "Admin"){
-       console.log("hello admin, would you like to see a status report?");
+//• Remove all of the usernames from your array, and make sure the correct message is printed.
+
+
+let users=[];
+
+if(users.length >0){
+    for(var i = 0 ; i <users.length ; i++){
+        console.log("Hello," + users [i])
     }
-else {
-    console.log("hello" + username[i] + ". thank you for logging in again.");
-}
+}else{ 
+    console.log("We need to find some users! ");
 }
